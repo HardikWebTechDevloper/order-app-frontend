496dac33bf13c4a49ef0234acd99568233013884
Hardik Gadhiya <hardik.j@vibrant-info.com>
2021-10-08 15:45:41 +0530

    Created new builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   authenticated: false,
   hmr: false,
-  API_URL: 'http://13.232.213.216:5000/api/v1/'
+  API_URL: 'http://3.108.44.78:5000/api/v1/'
+  // API_URL: 'http://13.232.213.216:5000/api/v1/'
 }
